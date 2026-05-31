165c606650aed971dd716d9fa8cae998cbac2df4
mishotiman <62565728+mishotiman@users.noreply.github.com>
2026-05-31 16:34:29 +0300

    Fixed some test. Added Prettier (automatically wrapped/formatted code). Changed Image src to CloudFront

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+    images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "dzpz47k6dvo72.cloudfront.net",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
